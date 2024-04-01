e2d8bc8cb284a1a4ecdb708ccd99f6b7ce8f75e2
Turobjon <avezovzamir337@gmail.com>
2024-04-01 19:44:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism =prompt('ism' )
+let familya =prompt('familya' )
+let maktab =prompt('maktab' )
+let sinf =prompt('sinf' )
+let manzil =prompt('manzil' )
+
+
+
+console.log(ism)
+console.log(familya)
+console.log(maktab)
+console.log(sinf)
+console.log(manzil)
+console.log(natija)
